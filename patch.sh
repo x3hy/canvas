d091d986dab47a9a6ac4a30b5ba39ada52f00bf3
Lucy <tkf.x1os@gmail.com>
2026-03-23 17:23:12 +1300

    fixed patch script

--- patch.sh
+++ patch.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
+rm canvas.diff
 mv .git .hide_git
 cd dwm_patched
 git clone https://git.suckless.org/dwm
 mv dwm/.git .
 rm -r dwm
-git diff > ../canvas.diff
+git add .
+git diff --staged > ../canvas.diff
 rm -rf .git
 cd -
 mv .hide_git .git
