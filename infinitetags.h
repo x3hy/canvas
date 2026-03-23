bafa74015f71827070047be6356ad0a8e16312d9
Lucy <tkf.x1os@gmail.com>
2026-03-23 13:36:36 +1300

    base

---
+++ infinitetags.h
@@ -0,0 +1,7 @@
+static void movecanvas(const Arg *arg);
+static void manuallymovecanvas(const Arg *arg);
+static void homecanvas(const Arg *arg);
+static int getcurrenttag(Monitor *m);
+static void save_canvas_positions(Monitor *m);
+static void restore_canvas_positions(Monitor *m);
+static void centerwindow(const Arg *arg);
